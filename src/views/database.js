772c899daa320d2431da936f959e4b71d1295fd2
Mariojoestar33 <mariano.pena15@gmail.com>
2024-01-22 14:50:42 -0600

    base de datos

--- src/views/database.js
+++ src/views/database.js
@@ -1,11 +1,24 @@
-const mongoose = require('mongoose')
+const {MongoClient} = require('mongoose')
 
-const mongoDB_URI = 'mongodb://localhost:27017//upiita'
+const uri = 'mongodb://localhost:27017/upiita'
 
-mongoose.connect(mongoDB_URI)
 
-const conn = mongoose.connection
-
-conn.once('open', () => {
-    console.log('Conexion correcta a la base de datos')
+const client = new MongoClient(uri,  {
+    serverApi: {
+        version: '1'
+    }
 })
+
+async function run() {
+    try {
+      // Connect the client to the server (optional starting in v4.7)
+      await client.connect()
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close()
+    }
+  }
+  run().catch(console.dir)
