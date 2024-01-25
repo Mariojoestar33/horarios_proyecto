f1cac50dcbb75feed3787d198ebac4138091f30b
Mariojoestar33 <mariano.pena15@gmail.com>
2024-01-25 10:04:52 -0600

    Add @types/node dependency and update package-lock.json

--- src/server/routes/materias.js
+++ src/server/routes/materias.js
@@ -3,6 +3,29 @@ const { client } = require("../database/database")
 const { materia, database } = require("../models/materiamodel")
 const config = require("../config/config")
 
+// Ruta para index
+router.get('/', async (req, res) => {
+  try {
+    await client.connect()
+    await database.command({ ping: 1 })
+    console.log("Conexion EXITOSA a Cluster")
+    //let query = {}
+    //let materias = await materia.find(query).toArray()
+    const materias = await materia.distinct('Asignatura')
+    const profesores = await materia.distinct('Profesor')
+    if (materias.length === 0) {
+      console.log("No se pudieron cargar las materias")
+      res.status(404).json({ mensaje: "No se pudieron cargar las materias"})
+    } else {
+      console.log("Datos recogidos EXITOSAMENTE")
+      res.render('index', { pageTitle: "Materias", materias, profesores })
+      //res.json(materias)
+    }
+  } finally {
+    await client.close()
+  }
+})
+
 // Ruta para obtener todas las materias
 router.get('/materias', async (req, res) => {
     try {
@@ -12,19 +35,40 @@ router.get('/materias', async (req, res) => {
       //let query = {}
       //let materias = await materia.find(query).toArray()
       const materias = await materia.distinct('Asignatura')
-      const profesores = await materia.distinct('Profesor')
       if (materias.length === 0) {
         console.log("No se pudieron cargar las materias")
         res.status(404).json({ mensaje: "No se pudieron cargar las materias"})
       } else {
         console.log("Datos recogidos EXITOSAMENTE")
-        res.render('index', { pageTitle: "Materias", materias, profesores })
-        //res.json(materias)
+        //res.render('index', { pageTitle: "Materias", materias, profesores })
+        res.json(materias)
       }
     } finally {
       await client.close()
     }
 })
+
+//Ruta para obtener la materia especifica
+router.get(`/materia`, async (req, res) => {
+  try {
+    const buscar = req.query.materia
+    await client.connect()
+    await client.db(config.dbName).command({ ping: 1 })
+    console.log("Conexion EXITOSA a Cluster")
+    let query = {Asignatura: `${buscar}`}
+    let materias = await materia.find(query).toArray()
+    if (materias.length === 0) {
+      console.log("No se encontraron datos para la materia especificada")
+      res.status(404).json({ mensaje: 'No se encontraron datos para la materia especificada' })
+  } else {
+          console.log("Datos recogidos EXITOSAMENTE")
+          res.json(materias)
+      }
+  } finally {
+    await client.close()
+  }
+})
+
 
 //Ruta para obtener la materia especifica
 router.get(`/materias/:materia`, async (req, res) => {
